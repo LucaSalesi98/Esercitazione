0b7e82c9154a5df3a8d9f127578b65161e0db978
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-02-04 12:19:32 +0100

    modified files

--- eserci.js
+++ eserci.js
@@ -1,3 +1,14 @@
 //test
 
-prompt('write a number');
+var myNumber = prompt('write a number');  // prompt rialascia una stringa 
+myNumber =parseInt(myNumber);
+
+if(myNumber < 0) {
+    alert('given number is negative.');
+    myNumber = Math.abs(myNumber);
+} else{
+    alert('given number is positive');
+}
+var squareRoot = Math.sqrt(myNumber);
+
+alert(squareRoot);
