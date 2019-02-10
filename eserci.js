e2ebefd893ee6f15ee3a107b899a9bf8de4adf8b
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-02-10 20:32:33 +0100

    esercizi per lunedi 11

--- eserci.js
+++ eserci.js
@@ -1,14 +1,38 @@
-//test
+//Letto un numero, far stampare un messaggio in caso il numero sia uguale a 10.
 
-var myNumber = prompt('write a number');  // prompt rialascia una stringa 
+
+/*
+var myNumber = prompt('write a number');
 myNumber =parseInt(myNumber);
 
-if(myNumber < 0) {
-    alert('given number is negative.');
-    myNumber = Math.abs(myNumber);
-} else{
-    alert('given number is positive');
-}
-var squareRoot = Math.sqrt(myNumber);
+if(myNumber == 10) {
+    alert('given number is 10');
+} else {
+    alert('given number is not 10');
+};
+*/
 
-alert(squareRoot);
+//Letti due numeri, far stampare un messaggio in caso siano uguali tra di loro.
+/*
+var firstNumber = prompt ('write the first number');
+var secondNumber = prompt ('write the second number');
+
+firstNumber = parseInt(firstNumber);
+secondNumber = parseInt(secondNumber);
+
+if(firstNumber == secondNumber){
+    alert('you wrote the same number twice');
+} else {
+    alert('you wrote two different numbers')
+};
+*/
+
+var firstString = prompt ('write something');
+var secondString = prompt ('write the same thing');
+
+if (firstString == secondString){
+    alert('congratulation! you can write the same thing twice!')
+} else{
+    alert('i said the same thing! you wrote two different things :(')
+}
+
