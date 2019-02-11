73f05f2c95c8a59aa3aecbbe259572354f9fa256
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-02-11 09:59:50 +0100

    compiti corretti

--- eserci.js
+++ eserci.js
@@ -5,11 +5,11 @@ var myNumber = prompt('write a number');
 
 myNumber =parseInt(myNumber);
 
-    if(myNumber == 10) {
-        alert('given number is 10');
-          } else {
-        alert('given number is not 10');
-         };
+if(myNumber == 10) {
+    alert('given number is 10');
+      } else {
+    alert('given number is not 10');
+      };
 
 
 //Letti due numeri, far stampare un messaggio in caso siano uguali tra di loro.
@@ -20,20 +20,20 @@ var secondNumber = prompt ('write the second number');
 firstNumber = parseInt(firstNumber);
 secondNumber = parseInt(secondNumber);
 
-    if(firstNumber == secondNumber){
-        alert('you wrote the same number twice');
-         } else {
-        alert('you wrote two different numbers')
-        };
+if(firstNumber == secondNumber){
+    alert('you wrote the same number twice');
+      } else {
+    alert('you wrote two different numbers')
+    };
 
 // Lette due stringhe, far stampare un messaggio in caso risultino uguali tra di loro e un messaggio alternativo in caso contrario.
 
 var firstString = prompt ('write something');
 var secondString = prompt ('write the same thing');
 
-    if (firstString == secondString){
-        alert('congratulation! you can write the same thing twice!')
-         } else{
-        alert('i said the same thing! you wrote two different things :(')
-         }
+if (firstString == secondString){
+    alert('congratulation! you can write the same thing twice!')
+      } else{
+    alert('i said the same thing! you wrote two different things :(')
+      }
 
