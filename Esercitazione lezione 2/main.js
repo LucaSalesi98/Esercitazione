d02b87959ffddec97a086546cc33f8d556d46e6d
LucaSalesi98 <lucasalesi.1d@gmail.com>
2019-02-11 11:52:27 +0100

    Programmazione a 'matrioska'  con varie funzioni

--- Esercitazione lezione 2/main.js
+++ Esercitazione lezione 2/main.js
@@ -2,6 +2,39 @@
 //differenza fra var e const: const non è modificabile una volta dichiarata, se dichiarata come in questo caso uguale a 10, rimarra sempre uguale a 10
 //      e si scrive in UPPERCASE ovvero tutto maiuscolo con l'utilizzo del trattino basso 
 
+//FUNCTION    
+
+function readNumber() {
+    var myNumber = prompt('insert a number:'); 
+    myNumber = parseInt(myNumber);
+    return myNumber;
+}
+
+function printMessage(message) {
+    var myMessage = message;
+    alert (myMessage);
+    return;
+}
+
+function isEqualeToTen(myNumber){
+    const COMPARABLE_NUMBER = 10;
+    return myNumber == COMPARABLE_NUMBER;
+
+}
+
+function simpleCheck(){
+var myNumber = readNumber();
+var isThatNumberEqualToTen = isEqualeToTen(myNumber);
+
+if(isThatNumberEqualToTen){
+    printMessage('YES, the number is equal to ten');
+}
+
+}
+
+simpleCheck();
+
+/*
 const COMPARABLE_NUMBER = 10;
 var readedNumber = prompt('insert a number:');
 
@@ -10,3 +43,4 @@ readedNumber = parseInt(readedNumber);
 if(readedNumber == COMPARABLE_NUMBER ){
     alert("paragone OK");
 }
+*/
